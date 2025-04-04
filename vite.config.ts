b95e7b548d25dfa5d6acf0ea0c697ac51a0ccb5b
MD. RAKIBUL HASAN <aporbo.hasan@gmail.com>
2025-04-04 17:30:33 -0400

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing-ai-agent/',
+  base: '/spectra-ai-landing/',
 }) 
